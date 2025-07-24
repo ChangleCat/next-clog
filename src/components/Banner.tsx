08da2e040f01c19fb29755949cb094392e845243
ChangleCat <513618438@qq.com>
2025-07-24 12:29:50 +0800

    feat: Header & Banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,9 +4,9 @@ import { cn } from "@/utils/cn"
 export function Banner() {
 
     return (
-        <div className={cn("absolute top-0 left-0 w-full h-[100svh] overflow-hidden -z-1",
-            "after:bg-gradient-to-b after:from-transparent after:to-surface-1 after:relative after:z-10")}>
-            <Image src="https://blog-images.s3.bitiful.net/banner-alice.jpg" alt="banner" fill={true} objectFit="cover" />
+        <div className={cn("absolute top-0 left-0 w-full h-[100svh] overflow-hidden -z-1")}>
+            <Image src="https://blog-images.s3.bitiful.net/banner-alice.jpg" alt="banner" fill className="object-cover"/>
+            <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-black/10 h-full w-full"></div>
         </div>
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,76 @@
-import Link from "next/link";
+'use client';
 
+import Link from "next/link";
+import { Icon } from "@iconify/react";
+import { cn } from "@/utils/cn";
+import { EventHandler, use, useEffect, useState } from "react";
 
 export function Header() {
-    return (
-        <header className="bg-surface-2 w-full p-4 shadow-md flex justify-between items-center rounded-b-md text-[18px]">
-            <div><Link href={"#"} className="rounded-sm hover:bg-accent-gold p-3">人偶使の小屋</Link></div>
-            <nav className="hidden md:block">
-                <ul className="flex gap-4 ">
-                    <li><Link href={"/"}>首页</Link></li>
-                    <li><Link href={"/posts"}>归档</Link></li>
-                    <li><Link href={"/tags"}>标签</Link></li>
-                    <li><Link href={"/categories"}>分类</Link></li>
-                    <li><Link href={"/now"}>如今</Link></li>
-                    <li><Link href={"/about"}>关于</Link></li>
-                </ul>
-            </nav>
-            <div>
+    const [isTop, setIsTop] = useState(true);
+    const [isDarkMode, setIsDarkMode] = useState(false);
 
+    const themeChange = (isDark: boolean) => {
+        document.documentElement.classList.toggle("dark", isDark);
+        setIsDarkMode(isDark);
+        localStorage.setItem("theme", isDark ? "dark" : "light");
+    }
+
+    const handleThemeToggle = () => {
+        themeChange(!isDarkMode);
+    }
+
+    useEffect(() => {
+        // 主题选取
+        // 检查 localStorage 中是否有主题设置
+        const prevTheme = localStorage.getItem("theme");
+        if (prevTheme !== null) {
+            // 如果有，应用该主题
+            themeChange(prevTheme === "dark");
+        } else {
+            // 如果没有，检查用户的系统偏好
+            const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+            themeChange(prefersDark);
+        }
+
+        // 监听滚动事件，判断是否在顶部
+        const handleScroll = () => {
+            setIsTop(window.scrollY === 0);
+        };
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+
+    }, []);
+
+    return (
+        <header className={cn("bg-surface-2 w-full p-4 shadow-md rounded-b-md text-[18px] fixed top-0 z-1000",
+            "flex justify-center items-center",
+            isTop && "bg-surface-2/5 text-text-main-dark"
+        )}>
+            <div className="flex justify-between items-center max-w-7xl w-full">
+                <div><Link href={"/"} className="rounded-sm p-3 font-bold text-xl">人偶使の小屋</Link></div>
+                <nav className="hidden md:block">
+                    <ul className={cn("flex gap-4",
+                        "[&>li>a]:p-2 [&>li>a]:rounded-md [&>li>a]:flex [&>li>a]:items-center",
+                        "[&>li>a]:hover:bg-button-hover [&>li>a]:hover:text-text-main-light",)}>
+                        <li><Link href={"/"}>首页</Link></li>
+                        <li><Link href={"/posts"}>归档</Link></li>
+                        <li><Link href={"/tags"}>标签</Link></li>
+                        <li><Link href={"/categories"}>分类</Link></li>
+                        <li><Link href={"/now"}>如今</Link></li>
+                        <li><Link href={"/about"}>关于</Link></li>
+                    </ul>
+                </nav>
+                <div>
+                    <button type="button" className={cn("p-2 rounded-md hover:bg-button-hover transition-colors duration-200 cursor-pointer active:scale-95")}
+                        onClick={handleThemeToggle}>
+                        {isDarkMode ?
+                            <Icon icon="material-symbols:light-mode-rounded" /> :
+                            <Icon icon="material-symbols:dark-mode-rounded" />}
+                    </button>
+                </div>
             </div>
         </header>
     )
