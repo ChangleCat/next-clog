ce89def824b96b296e8b7125a065c41aad23878e
ChangleCat <513618438@qq.com>
2025-07-29 21:43:17 +0800

    fix: scroll-smooth适配未来版本

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="zh-CN" className="scroll-smooth">
+    <html lang="zh-CN" className="scroll-smooth" data-scroll-behavior="smooth">
       <head>
         <FontLoader />
       </head>
