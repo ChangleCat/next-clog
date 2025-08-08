7793ca0099c2aa05df103a4a84e4a12151e4e25c
ChangleCat <513618438@qq.com>
2025-08-08 18:00:11 +0800

    fix: 淡入淡出

--- src/components/TransitionProvider.tsx
+++ src/components/TransitionProvider.tsx
@@ -14,7 +14,7 @@ export default function TransitionProvider({ children }: Readonly<{
                 key={pathname}
                 initial={{ opacity: 0}}
                 animate={{ opacity: 1}}
-                // exit={{ opacity: 0 }}
+                exit={{ opacity: 0 }}
                 transition={{ duration: 0.5, ease: "easeInOut" }}
             >
                 {children}
